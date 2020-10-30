4069c7a52f2e363c0f1aae70e172ad559db0aece
ErinHe1114 <66896047+ErinHe1114@users.noreply.github.com>
2020-10-30 14:11:22 +1000

    Update forms.py

--- auction/forms.py
+++ auction/forms.py
@@ -1,6 +1,5 @@
-
 from flask_wtf import FlaskForm
-from wtforms.fields import TextAreaField,SubmitField, StringField, PasswordField
+from wtforms.fields import TextAreaField,SubmitField, StringField, PasswordField,SelectField
 from wtforms.validators import InputRequired, Length, Email, EqualTo
 
 
